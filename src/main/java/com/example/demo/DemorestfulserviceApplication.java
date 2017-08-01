272cfc492df1494dacfa7c7a75ef25fc6248835f
bennnnnn <bmecuriaw@gmail.com>
2017-08-01 17:13:41 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DemorestfulserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorestfulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorestfulserviceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/controller/GreetingContoller.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingContoller {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+        @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+
+    @RequestMapping("/addProduct")
+    public  String  addProduct(){
+
+
+        return  "Add a product here";
+    }
+
+
+    @RequestMapping("/listProducts")
+        public   String listProducts(){
+            return "THis is a list of your products.";
+
+        }
+
+        @RequestMapping("/thankYou")
+        public String thankYou(){
+            return  "Thank you for shopping with us.";
+        }
+
+
+
+}
+
+
+
